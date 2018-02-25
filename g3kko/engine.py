10c0dc70afe8fdc029a16cf0c11c7d1cb0d9adca
cowboycod3r <cowboycod3r@gna13.de>
2018-02-25 23:31:34 +0100

    working example with mysql in docker

--- g3kko/engine.py
+++ g3kko/engine.py
@@ -3,7 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:tigger@127.0.0.1:3306/test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -11,18 +13,18 @@ db = SQLAlchemy(app)
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    order_date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
-    value_date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
+    order_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    value_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     principal = db.Column(db.Text, nullable=True)
     order_text = db.Column(db.Text, nullable=True)
     purpose = db.Column(db.Text, nullable=True)
     value = db.Column(db.Float, nullable=False)
-    currency = db.Column(db.String(3), nullable=False)
+    currency = db.Column(db.String(3), nullable=False, default='EUR')
     shares = db.Column(db.Float, nullable=True)
     isin = db.Column(db.String(12), nullable=True)
 
     def __repr__(self):
-        return '<Transaction %s>' % self.isin
+        return '<Transaction %i %s %f>' % (self.id, self.value_date, self.value)
 
 
 
@@ -52,12 +54,14 @@ def transactions():
 @app.route('/transactions/test')
 def transactions_create_test_data():
     db.create_all()
-    t1 = Transaction(value=100, currency='EUR')
-    t2 = Transaction(value=200, currency='EUR')
+    t1 = Transaction(value=100.24)
+    t2 = Transaction(value=200.23)
     db.session.add(t1)
     db.session.add(t2)
     db.session.commit()
-
+    r = Transaction.query.all()
+    for t in r:
+        print(t)
     return 'done.'
 
 @app.route('/fund')
