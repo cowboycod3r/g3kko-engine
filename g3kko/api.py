be0022155fb8370d0c4ac9cdd7dcc0cd973d7214
cowboycod3r <cowboycod3r@gna13.de>
2018-03-05 21:13:41 +0100

    updated the data model for a transaction

--- g3kko/api.py
+++ g3kko/api.py
@@ -2,19 +2,32 @@ from g3kko import db
 from datetime import date
 
 
-# Buchung;Valuta;Auftraggeber/Empfänger;Buchungstext;Verwendungszweck;Betrag;Währung;Saldo;Währung
 
 class Transaction(db.Model):
+    """Model for a Transaction
+
+    Mapping for CSV import:
+    date: Valuta
+    text: Auftraggeber, Buchungstext, Verwendungszweck
+    purpose: calculation from text
+    currency: Waehrung
+    value: Betrag
+    shares: has to be updated by the user
+    isin: calculation from text
+
+
+    """
     id = db.Column(db.Integer, primary_key=True)
-    order_date = db.Column(db.Date, nullable=False, default=date.today)
-    value_date = db.Column(db.Date, nullable=False, default=date.today)
-    principal = db.Column(db.Text, nullable=True)
-    order_text = db.Column(db.Text, nullable=True)
-    purpose = db.Column(db.Text, nullable=True)
+    date = db.Column(db.Date, nullable=False, default=date.today)
+    text = db.Column(db.Text, nullable=True)
+    purpose = db.Column(db.String(128), nullable=True)
     value = db.Column(db.Float, nullable=False)
-    currency = db.Column(db.String(3), nullable=False, default='USD')
+    currency = db.Column(db.String(3), nullable=False, default='EUR')
     shares = db.Column(db.Float, nullable=True)
     isin = db.Column(db.String(12), nullable=True)
 
+    def value_per_share(self):
+        return self.value/self.shares
+
     def __repr__(self):
-        return '<Transaction %i %s %f>' % (self.id, self.value_date, self.value)
+        return '<Transaction %i %s %f>' % (self.id, self.date, self.text, self.value)
